50ad675a07059f5000a042287b311a973311e810
Redha1996 <zatoutredha@gmail.com>
2021-12-23 21:43:46 +0100

    fix static field

--- prediction-engine/src/main/java/fr/lernejo/prediction/utils/DateUtils.java
+++ prediction-engine/src/main/java/fr/lernejo/prediction/utils/DateUtils.java
@@ -5,20 +5,18 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateUtils {
-    private static final long ONE_DAY_MILLI_SECONDS = 86400000;
-
     private DateUtils() {
     }
 
     public static String oneDayBefore(Date date) {
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        Date newDate = new Date(date.getTime() - ONE_DAY_MILLI_SECONDS);
+        Date newDate = new Date(date.getTime() - 86400000);
         return dateFormat.format(newDate);
     }
 
     public static String twoDaysBefore(Date date) {
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        Date newDate = new Date(date.getTime() - (2 * ONE_DAY_MILLI_SECONDS));
+        Date newDate = new Date(date.getTime() - (2 * 86400000));
         return dateFormat.format(newDate);
     }
 }
